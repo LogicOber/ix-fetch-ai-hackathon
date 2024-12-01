aa6e896d3e0332199972d42f753b9874673715f4
LOS <133167749+LogicOber@users.noreply.github.com>
2024-12-01 11:34:38 +0000

    file fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,14 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    host: true, // 监听所有地址
+    port: 8080,
+    strictPort: true, // 端口被占用时抛出错误
+  },
+  preview: {
+    host: true,
+    port: 8080,
+    strictPort: true,
+  }
 });
